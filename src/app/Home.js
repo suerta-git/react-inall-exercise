71e5702bebace4b7a2adab3b25008ffe38bf3c0c
Suerta <suerta__@hotmail.com>
2020-09-23 21:02:04 +0800

    [Zhang Jindong] feat: impletement home page

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import './home.less';
+import {Link} from 'react-router-dom';
+import './Home.less';
 
 const Home = () => {
   return (<div className="home">
-    This is home page
+    <section id="title">
+      <h1>在线实用工具</h1>
+    </section>
+    <section id="links">
+      <ul>
+        <li id="calculator">
+          <img className="image-size" src={require('../images/calculator.png')} alt="calculator" />
+          <Link to="/calculator">计算器</Link>
+        </li>
+        <li id="timer">
+          <img className="image-size" src={require('../images/timer.png')} alt="timer" />
+          <Link to="/timer">倒计时器</Link>
+        </li>
+      </ul>
+    </section>
   </div>);
 };
 
