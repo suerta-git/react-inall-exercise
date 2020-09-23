bd4f98d8a442dbcb3348a7493479e8e96c21f93f
Suerta <suerta__@hotmail.com>
2020-09-24 00:02:39 +0800

    [Zhang Jindong] feat: impletement function

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import {Route, BrowserRouter} from "react-router-dom";
 import Home from "./Home";
 import Header from './Header';
 import Calculator from './calculator/Calculator';
+import Timer from './timer/Timer';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Header />
           <Route exact path='/' component={Home}/>
           <Route path='/calculator' component={Calculator} />
+          <Route path='/timer' component={Timer} />
         </BrowserRouter>
       </div>
     );
---
+++ src/app/timer/Timer.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Timer extends React.Component {
+  state = {
+    seconds: '',
+    display: 'Start',
+    timing: false
+  }
+
+  componentWillUnmount = () => {
+    this.setState({
+      seconds: '',
+      display: 'Start',
+      timing: false
+    });
+  }
+
+  render() {
+    return (
+      <section className="page" id="timer">
+        <h1>在线倒计时器</h1>
+        <div>
+          <form>
+            <label for="time">设置时间</label>
+            <input type="text" id="time" value={this.state.seconds} onChange={this.handleInputChange} />
+            <button type="button" onClick={this.handleStart} disabled={this.state.timing}>Start</button>
+          </form>
+          <p>{this.state.display}</p>
+        </div>
+        <Link to="/">回到主页</Link>
+      </section>
+    );
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      seconds: event.target.value
+    });
+  }
+
+  handleStart = () => {
+    let seconds = Number(this.state.seconds);
+    this.setState({
+      seconds: ''
+    });
+
+    if (Number.isNaN(seconds) || seconds <= 0) {
+      return;
+    }
+
+    this.setState({
+      timing: true,
+      display: `${seconds} Seconds`,
+    });
+
+    const id = setInterval(() => {
+      this.setState({
+        display: `${--seconds} Seconds`,
+      });
+      if (seconds === 0) {
+        this.setState({
+          timing: false,
+          display: 'End'
+        });
+        clearInterval(id);
+      }
+    }, 1000);
+  }
+};
